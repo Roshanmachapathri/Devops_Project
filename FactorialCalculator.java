3bb9a7edaa5d04cc7afe40ed0f963f5878d19252
roshan <53788563+Roshanmachapathri@users.noreply.github.com>
2024-04-26 16:52:40 +0530

    Update FactorialCalculator.java

--- FactorialCalculator.java
+++ FactorialCalculator.java
@@ -3,22 +3,20 @@ import java.util.Scanner;
 public class FactorialCalculator {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+
         System.out.print("Enter a number to calculate its factorial: ");
         int number = scanner.nextInt();
-        scanner.close();
 
-        long factorial = calculateFactorial(number);
+        int factorial = calculateFactorial(number);
         System.out.println("Factorial of " + number + " is: " + factorial);
+
+        scanner.close();
     }
 
-    public static long calculateFactorial(int n) {
-        if (n == 0 || n == 1) {
+    public static int calculateFactorial(int n) {
+        if (n == 0)
             return 1;
-        }
-        long factorial = 1;
-        for (int i = 2; i <= n; i++) {
-            factorial *= i;
-        }
-        return factorial;
+        else
+            return n * calculateFactorial(n - 1);
     }
 }
